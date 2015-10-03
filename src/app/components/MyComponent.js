1bd5e77edd83635a37d8799a913ecf7995db9c4f
Jinggo <jinggo.villamor@gmail.com>
2015-10-04 06:53:18 +0800

    add branch components only

---
+++ src/app/components/MyComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+	
+	state = {};
+	
+	static defaultProps = {};
+
+	render() {
+		return (
+			<div>My Components Here</div>
+		);
+	}
+}
+
+export default MyComponent;
--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,4 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import configureStore from './stores/configureStore';
+import MyComponent from './components/MyComponent';
 
-//PageContainers
-import App from './containers/App';
-
-const store = configureStore();
-
-React.render(
-	<Provider store={store}>
-		{() => 
-			<App/>
-		}
-	</Provider>,
-	document.getElementById('app')
-)
+React.render(<MyComponent/>, document.getElementById('app'));
